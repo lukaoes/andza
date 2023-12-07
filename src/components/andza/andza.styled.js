575ecb0d55c83df88a2e329127b70b47879cfba5
Luka Pitskhelauri <luka.pitsk@gmail.com>
2023-12-07 19:07:59 +0400

    Add middle thick connections of Andza.

--- src/components/andza/andza.styled.js
+++ src/components/andza/andza.styled.js
@@ -2,15 +2,19 @@ import styled from "styled-components";
 
 export const AndzaLayout = styled.div`
     height: 500px;
-    width: 100px;
+    width: 200px;  // old was 100
     background-color: gray;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 25px;
 `
 
 export const ThickBottom = styled.div`
     height: 125px;
     width: 10px;
     position: relative;
-
+    margin-right: 4px;
     &:after, &:before { 
         content:' ';
         position: absolute;
@@ -19,18 +23,20 @@ export const ThickBottom = styled.div`
     }
 
     &:after {top: 0px; background: white;  }
-    &:before {bottom: 0px; background: rgb(245 89 88);}
+    &:before {bottom: 0px; background: #F55958;}
 `
 
 export const ThickMiddle = styled.div`
     height: 130px;
     width: 8px;
-
+    margin-right: 4px;
+    border-top-left-radius: 15%;
+    border-top-right-radius: 15%;
     background: linear-gradient(to bottom,
         white 23%,
         white 23%,
-        rgb(245 89 88) 23%,
-        rgb(245 89 88) 83%,
+        #F55958 23%,
+        #F55958 83%,
         white 83%,
         white 100%
     );
@@ -40,15 +46,37 @@ export const ThickMiddle = styled.div`
 export const ThickTop = styled.div`
     height: 185px;
     width: 6px;
-
+    margin-right: 4px;
+    border-top-left-radius: 100%;
+    border-top-right-radius: 100%;
     background: linear-gradient(to bottom,
     white 14%,
-    rgb(245 89 88) 14%,
-    rgb(245 89 88) 41%,
+    #F55958 14%,
+    #F55958 41%,
     white 41%,
     white 51%,
-    rgb(245 89 88) 51%,
-    rgb(245 89 88) 83%,
+    #F55958 51%,
+    #F55958 83%,
     white 83%
   )
+`
+
+export const MiddleThickConnectBottom = styled.div`
+    width: 42px;
+    height: 5px;
+    border: 4px solid 	#F55958;
+    border-radius: 2px;
+    margin-right: 33px;
+    background-color: white;
+`
+
+export const MiddleThickConnectTop = styled.div`
+    width: 35px;
+    height: 25px;
+    background-color: white;
+    border-top-right-radius: 30%;
+    border-top-left-radius: 30%;
+    border-bottom-right-radius: 70%;
+    border-bottom-left-radius: 70%;
+    margin-right: 4.5px;
 `
--- src/components/andza/index.js
+++ src/components/andza/index.js
@@ -1,11 +1,15 @@
 import { AndzaLayout, ThickBottom, ThickMiddle, ThickTop } from "./andza.styled"
+import { MiddleThickConnectBottom, MiddleThickConnectTop } from "./andza.styled"
+
 
 const Andza = () => {
 
     return (
         <AndzaLayout>
             <ThickTop></ThickTop>
+            <MiddleThickConnectTop></MiddleThickConnectTop>
             <ThickMiddle></ThickMiddle>
+            <MiddleThickConnectBottom></MiddleThickConnectBottom>
             <ThickBottom></ThickBottom>
         </AndzaLayout>
     )

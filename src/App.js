79fa21929969ee40207df7e162e97228d30a6073
Luka Pitskhelauri <luka.pitsk@gmail.com>
2023-12-08 20:06:39 +0400

    Add function, that changes sky gradient class depending what time is it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import Andza from "./components/andza";
 import Mta from "./components/mta";
 import './components/sky/index.scss'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [currentClass, setCurrentClass] = useState("01");
+
+  useEffect(() => {
+    const updateSkyClass = () => {
+      const currentTime = new Date();
+      const hour = currentTime.getHours();
+
+      let newClass;
+      if (hour === 0) {
+        newClass = '24';
+      } else if (hour >= 1 && hour <= 23) {
+        newClass = hour.toString().padStart(2, '0');
+      }
+
+      setCurrentClass(newClass);
+    };
+
+    updateSkyClass();
+
+    const intervalId = setInterval(updateSkyClass, 60000);
+
+    return () => clearInterval(intervalId);
+  }, [currentClass]);
+
   return (
-    <div className="sky-gradient-07 vh-100">
+    <div className={`sky-gradient sky-gradient-${currentClass}`}>
       <Andza />
       <Mta> </Mta>
     </div>
